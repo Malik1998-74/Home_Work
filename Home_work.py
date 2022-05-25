4cad5a3eaa54318c78ab72ede6abba24bb043cf9
Malik Nizamov <mn@python.ru>
2022-05-25 20:11:12 +0300

    My hone work

--- Home_work.py
+++ Home_work.py
@@ -3,12 +3,38 @@
 # Пройдите в цикле по списку ['Вася',' Маша', 'Петя',' Валера', 'Саша', 'Даша'] пока не встретиться имя 'Валера'.
 # Когда найдете напишите 'Валера нашелся'
 
-list = ['Вася',' Маша', 'Петя',' Валера', 'Саша', 'Даша']
-print(list)
-while True:
-    names = input('Введите имя: ').capitalize()
-    if names == 'Валера':
-        print('Валера нашелся')
-        break
-    else:
-        print('Имя не найдено')
+# list = ['Вася',' Маша', 'Петя',' Валера', 'Саша', 'Даша']
+# print(list)
+# while True:
+#     names = input('Введите имя: ').capitalize()
+#     if names == 'Валера':
+#         print('Валера нашелся')
+#         break
+#     else:
+#         print('Имя не найдено')
+
+# Exercise 2
+# def find_peson(name):    
+#     while True:
+#         user_say = input('Скажи что-нибудь: ')
+#         if user_say == 'Пока':
+#             print('Ну пока')
+#             break
+#         else:
+#             print('Сам ты {}'.format(user_say))
+
+# Exercise 3
+# def ask_user():
+#     while True:
+#         state = input('Как дела?: ').capitalize()
+#         if state == 'Хорошо':
+#             break
+#         else:
+#             print('Повторяю вопрос')    
+# ask_user()
+
+# Exercise 4
+def get_answer(ask_user):
+    while True:
+        ask_user()
+get_answer()
