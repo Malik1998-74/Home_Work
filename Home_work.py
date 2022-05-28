15f7b5dbd4f00d06404f50261c57c685bffdc297
Malik Nizamov <mn@python.ru>
2022-05-28 18:35:37 +0300

    Home_work

--- Home_work.py
+++ Home_work.py
@@ -3,38 +3,43 @@
 # Пройдите в цикле по списку ['Вася',' Маша', 'Петя',' Валера', 'Саша', 'Даша'] пока не встретиться имя 'Валера'.
 # Когда найдете напишите 'Валера нашелся'
 
-list = ['Вася',' Маша', 'Петя',' Валера', 'Саша', 'Даша']
-print(list)
-while True:
-    names = input('Введите имя: ').capitalize()
-    if names == 'Валера':
+names = ['Вася',' Маша', 'Петя', 'Валера', 'Саша', 'Даша']  
+
+# for element in names:
+#     if element == 'Валера':
+#         print('Валера нашелся')
+#         break  # закончить цикл
+while len(names) > 0:
+    name = names.pop() 
+    if name == 'Валера':
         print('Валера нашелся')
-        break  # закончить цикл
-    else:
-        print('Имя не найдено')
+        break
+
+
+
 
 # Exercise 2
-def find_peson(name):    
-    while True:
-        user_say = input('Скажи что-нибудь: ')
-        if user_say == 'Пока':
-            print('Ну пока')
-            break
-        else:
-            print('Сам ты {}'.format(user_say))
+# def find_peson(name):    
+#     while True:
+#         user_say = input('Скажи что-нибудь: ')
+#         if user_say == 'Пока':
+#             print('Ну пока')
+#             break
+#         else:
+#             print('Сам ты {}'.format(user_say))
 
-# Exercise 3
-def ask_user():
-    while True:
-        state = input('Как дела?: ').capitalize()
-        if state == 'Хорошо':
-            break
-        else:
-            print('Повторяю вопрос')    
-ask_user()
+# # Exercise 3
+# def ask_user():
+#     while True:
+#         state = input('Как дела?: ').capitalize()
+#         if state == 'Хорошо':
+#             break
+#         else:
+#             print('Повторяю вопрос')    
+# ask_user()
 
-# Exercise 4
-def get_answer(ask_user):
-    while True:
-        ask_user()
-get_answer()
+# # Exercise 4
+# def get_answer(ask_user):
+#     while True:
+#         ask_user()
+# get_answer()
